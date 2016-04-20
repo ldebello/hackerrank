2936404b0ece3c8a07b0a751c99d460a167f6453
Luis de Bello <luis.debello@mulesoft.com>
2016-04-20 02:59:52 -0300

    Adding solution for solve me first

---
+++ src/main/scala/com/hacerkrank/algorithms/warmup/solve_me_first/Solution.scala
@@ -0,0 +1,8 @@
+package com.hacerkrank.algorithms.warmup.solve_me_first
+
+object Solution {
+
+  def main(args: Array[String]) {
+    println(io.Source.stdin.getLines().take(2).map(_.toInt).sum)
+  }
+}
