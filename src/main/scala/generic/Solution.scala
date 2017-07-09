b8ec2970967d0a49419965ce600660732a3eedf5
Luis De Bello <luis.debello@mulesoft.com>
2017-07-09 18:45:00 -0300

    Solution for finding minimum number of operation to reach N from 0

--- src/main/scala/generic/Solution.scala
+++ src/main/scala/generic/Solution.scala
@@ -6,7 +6,19 @@ object Solution {
     val lines = io.Source.stdin.getLines()
     val numberOfTestCases = lines.next().toInt
     for (index <- 0 until numberOfTestCases) {
-      val number = lines.next().toInt
+      var currentNumber = lines.next().toInt
+
+      var counter = 0;
+      while(currentNumber > 0) {
+        if (currentNumber % 2 != 0) {
+          currentNumber = currentNumber - 1
+        } else {
+          currentNumber = currentNumber / 2
+        }
+        counter = counter + 1
+      }
+
+      println(counter)
     }
   }
 }
